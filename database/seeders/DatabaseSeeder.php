628b8d1061b7bc2bb4e539128db0ca35e05b5854
Sushantpoojary14 <spoojari694@gmail.com>
2023-08-11 12:47:51 +0530

    .

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(2)->create();
+        // User::factory(2)->create();
         Admin::factory(1)->create();
 
         // $tsc =  TestSeries::factory()->create([
